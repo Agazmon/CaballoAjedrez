fd7dcf2528f3387b1a4debd493490f10d7ffcdf5
Alex Gázquez <ercic@Zero-Two>
2018-11-14 20:06:14 +0100

    Creados los métodos get y set para los atributos de Posicion

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -1,6 +1,27 @@
 package org.iesalandalus.programacion.caballoajedrez;
-
+import org.iesalandalus.programacion.utilidades.Entrada;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("La fila introducida no es válida, porfavor utilice otra entre 1 y 8.");
+		}else {
+			this.fila = fila;
+		}	
+	}
+	public int getFila() {
+		return this.fila;
+	}
+	public void setColumna(char columna) {
+		if (columna < 'a' && columna > 'h' || columna < 'A' && columna > 'H') {
+			throw new IllegalArgumentException("La columna introducida no es válida, porfavor utilice otra entre A y H");
+		}else {
+			this.columna = columna;
+		}
+	}
+	public char getColumna() {
+		return this.columna;
+	}
 }
